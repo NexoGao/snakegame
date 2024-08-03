0d045eac0a942f536f53a337056dbb6cda95f916
Anthony <anthony.buhot@epfl.ch>
2024-08-04 00:15:46 +0200

    Change Pos.cpp

--- Pos.cpp
+++ Pos.cpp
@@ -2,4 +2,25 @@
 // Created by cubin on 8/4/2024.
 //
 
-#include "Pos.h"
+#include "GameDesign.h"
+
+void Pos::setPos(int x, int y) {
+    this->x = x;
+    this->y = y;
+}
+
+void Pos::setPos(Pos pos) {
+    this->x = pos.getX();
+    this->y = pos.getY();
+}
+
+int Pos::getX() const {
+    return x;
+}
+
+int Pos::getY() const {
+    return y;
+}
+
+
+
