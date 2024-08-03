e89a50db109ff954972b3a661688ae20a794ee4d
Anthony <anthony.buhot@epfl.ch>
2024-08-04 00:16:17 +0200

    Global push

--- Snake.h
+++ Snake.h
@@ -4,11 +4,31 @@
 
 #ifndef SNAKE_H
 #define SNAKE_H
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
 
+#include "GameDesign.h"
 
 
 class Snake {
+public:
+    Snake();
 
+    void grow();
+    void move();
+    bool checkCollision() const;
+    void render(sf::RenderWindow &window);
+    void setDirection(Direction direction);
+    void getDirection();
+    sf::RectangleShape getSegment();
+
+private:
+    int size;
+    bool alive;
+    // Animation/Sprite Handler
+    Direction direction;
+    int life;
+    std::vector<sf::RectangleShape> segments;
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
+using namespace std;
+using namespace sf;
+
 int main() {
+    RenderWindow window(VideoMode(200,200), "Snake");
+    CircleShape shape(100.f);
+    shape.setFillColor(Color::Red);
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
